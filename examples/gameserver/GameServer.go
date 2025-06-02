05d2ec6e6e466e0eaab12c034288362aeb9ea57e
yh <408309839@qq.com>
2025-06-02 22:29:21 +0800

    fix

---
+++ examples/gameserver/GameServer.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	pitaya "ricebean/pkg"
+	"ricebean/pkg/config"
+	"ricebean/pkg/serialize/protobuf"
+)
+
+var app pitaya.Pitaya
+
+func main() {
+
+	svType := flag.String("type", "Game", "the server type")
+	serverMetadata := map[string]string{
+		"ipaddress": "192.168.1.100:1250", // Replace with actual public IP
+	}
+	cfg := config.NewDefaultPitayaConfig()
+
+	builder := pitaya.NewDefaultBuilder(false, *svType, pitaya.Cluster, serverMetadata, *cfg)
+	builder.Serializer = protobuf.NewSerializer() // 设置 Protobuf 序列化器 //builder.PacketDecoder//builder.MessageEncoder
+
+	app = builder.Build()
+	defer app.Shutdown()
+
+	//接收路由地址 server.service.handler
+	err := app.SetDictionary(map[string]uint16{
+		//---------Gateway------------------------------------------------
+		"LoginSvc.OnGatewayReceive": 1, //
+		"LobbySvc.OnGatewayReceive": 2, //
+		"ThemeSvc.OnGatewayReceive": 3, //
+		//"game.game.entergame":                  7,
+	})
+
+	if err != nil {
+		fmt.Printf("error setting route dictionary %s\n", err.Error())
+	}
+	app.Start()
+
+}
